6048e88cac89bb5d08e4df5dd9063e46264bbe4a
AlexLam12 <alex.e015@gmail.com>
2021-07-09 15:11:19 -0500

    get customer to show in dom

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -3,10 +3,12 @@ import { Route } from "react-router-dom"
 import { Home } from "./Home"
 import { AnimalProvider } from "./animal/AnimalProvider"
 import { AnimalList } from "./animal/AnimalList"
+import { CustomerProvider } from "./customers/CustomerProvider"
+import { CustomerList } from "./customers/CustomerList"
 // import { AnimalCard } from "./animal/AnimalCard"
 import { EmployeeCard } from "./employees/EmployeeCard"
 import { LocationCard } from "./locations/LocationCard"
-import { CustomerCard } from "./customers/CustomerCard"
+// import { CustomerCard } from "./customers/CustomerCard"
 
 export const ApplicationViews = () => {
     return (
@@ -31,9 +33,11 @@ export const ApplicationViews = () => {
                 <LocationCard />
             </Route>
 
-            <Route path="/customers">
-                <CustomerCard />
-            </Route>
+            <CustomerProvider>
+                <Route exact path="/customers">
+                    <CustomerList />
+                </Route>
+            </CustomerProvider>
         </>
     )
 }
---
+++ src/components/customers/CustomerList.js
@@ -0,0 +1,23 @@
+import React, { useContext, useEffect } from "react"
+import { CustomerContext } from "./CustomerProvider"
+import { CustomerCard } from "./CustomerCard"
+import "./Customer.css"
+
+export const CustomerList = () => {
+    const { customers, getCustomers } = useContext(CustomerContext)
+    useEffect(() => {
+        console.log("CustomerList: useEffect - getCustomer")
+        getCustomers()
+    }, [])
+
+    return (
+        <div className="customers">
+            {console.log("CustomerList: Render", customers)}
+            {
+                customers.map(customer => {
+                    return <CustomerCard key={customer.id} customer={customer}/>
+                })
+            }
+        </div>
+    )
+}
